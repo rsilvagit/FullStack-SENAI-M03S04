4528cfaccfeb8b952f39d6462d409b60542a8f35
Rafael Silva <rafael.sdasilva84@hotmail.com>
2023-08-24 19:01:05 -0300

    estrutura home

--- my-todo-list/src/pages/home/HomePage.jsx
+++ my-todo-list/src/pages/home/HomePage.jsx
@@ -1,7 +1,27 @@
+import CardComponent from '../../components/card/CardComponent'
+const todos = [
+    {
+        id: 1,
+        title: 'Tarefa 1',
+        description: 'yhskahkjhksjdhakjhdjkh',
+        status: false,
+    },
+    {
+        id: 2,
+        title: 'Tarefa 2',
+        description: 'yhskahkjhksjdhakjhdjkh',
+        status: true,
+    },
+]
+
 export const HomePage = () => {
     return (
-        <div>
-            HomePage
+        <div className="HomeContainer">
+            <div className="CardsContainer">
+                {todos.map(todo => 
+                    <CardComponent todo={todo} key={todo.id} />
+                )}
+            </div>
         </div>
     )
 }
