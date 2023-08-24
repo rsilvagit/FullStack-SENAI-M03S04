0ae50de316af7af9ced122ce2b1802b8900c87c9
Rafael Silva <rafael.sdasilva84@hotmail.com>
2023-08-24 10:40:35 -0300

    instalação de dependencias

--- Aula-1/visit-card/src/pages/home/HomePage.jsx
+++
@@ -1,8 +0,0 @@
-
-export const HomePage = () =>{
-    return(
-        <div className="Container">
-           <CardComponent/> 
-        </div>
-    )
-}
--- Aula-1/visit-card/src/routes/AppRoutes.jsx
+++
@@ -1,5 +0,0 @@
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import {HomePage} from '../pages/home/HomePage'
-import {ErrorPage} from '../pages/error/ErrorPage'
-
-export const 
