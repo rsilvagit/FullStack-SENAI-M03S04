1d3bc076165fdbd6f3e995080c665ee0d51fc337
Rafael Silva <rafael.sdasilva84@hotmail.com>
2023-08-24 14:43:59 -0300

    criando estrutura componentes da aplicação

---
+++ my-todo-list/src/components/button/ButtonComponent.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const ButtonComponent = props => {
+  return (
+    <div>ButtonComponent</div>
+  )
+}
+
+ButtonComponent.propTypes = {}
+
+export default ButtonComponent
