f1fc9ba8af79b0c4eaf31d9bcc6ebd8e28cbe110
Rafael Silva <rafael.sdasilva84@hotmail.com>
2023-08-24 12:05:43 -0300

    definição das rotas que vão usar o layout

--- my-todo-list/src/routes/AppRoutes.jsx
+++ my-todo-list/src/routes/AppRoutes.jsx
@@ -2,16 +2,18 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 import { ToDoPage } from "../pages/todo/ToDoPage"
 import { HomePage } from "../pages/home/HomePage"
 import { ErrorPage } from "../error/ErrorPage"
+import { Layout } from "../layouts/Layout"
 
 export const AppRoutes = () => {
     return (
         <Router>
             <Routes>
-                <Route path="/" element={<HomePage/>}/>
-                <Route path="/todo" element={<ToDoPage/>}/>
-                <Route path="/todo/:id" element={<ToDoPage/>}/>
-                <Route path="*" element={<ErrorPage/>}/>
-               
+                <Route path="/" element={<Layout />}>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/todo" element={<ToDoPage />} />
+                    <Route path="/todo/:id" element={<ToDoPage />} />
+                    <Route path="*" element={<ErrorPage />} />
+                </Route>
             </Routes>
         </Router>
     )
