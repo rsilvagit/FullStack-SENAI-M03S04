f53902bca5c7eccfe1fdeb5a614f588397ade547
Rafael Silva <rafael.sdasilva84@hotmail.com>
2023-08-24 16:11:17 -0300

    cardComponente- logica

--- my-todo-list/src/components/card/CardComponent.jsx
+++ my-todo-list/src/components/card/CardComponent.jsx
@@ -1,7 +1,50 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import ButtonComponent from '../button/ButtonComponent'
+import { MdEdit, MdDelete, MdAutorenew } from 'react-icons/md'
+import { useNavigate } from 'react-router-dom'
 
-export const CardComponent = () => {
-  return (
-    <div>CardComponent</div>
-  )
+const CardComponent = ({ todo }) => {
+    const { id, title, description, status } = todo;
+    const navigate = useNavigate();
+
+    const handleEdit= ()=>{
+        navigate(`/todo/${id}`);
+    };
+    const handleDelete= ()=>{
+        ///chamada do serviço de deleção
+    };
+    const changeStatus=()=>{
+        constNewStatus =!status;
+        //inserir serviço para alter o status do todo
+    };
+
+    return (
+        <div className='CardContainer'>CardComponent
+            <h3 className="Title">{title}</h3>
+            <p className='Description'>{description}</p>
+            <div className="ActionsContainer">
+                <ButtonComponent onClick={handleEdit}>
+                    <MdEdit />
+                </ButtonComponent>
+                <ButtonComponent onClick={handleDelete}>
+                    <MdDelete />
+                </ButtonComponent>
+                <ButtonComponent onClick={changeStatus}>
+                    <MdAutorenew />
+                </ButtonComponent>
+            </div>
+        </div>
+    )
 }
+
+CardComponent.propTypes = {
+    tod: PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        title: PropTypes.string.isRequired,
+        description: PropTypes.string,
+        status: PropTypes.bool,
+    }).isRequired,
+}
+
+export default CardComponent
